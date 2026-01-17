79911f8fc776d09b49b40c2ce0f79396cada127e
Seongjung Um <232974616+dragon6217@users.noreply.github.com>
2026-01-18 07:27:17 +0900

    feat: update to v2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World - v1", "status": "running"}
+    return {"message": "Hello World - v2", "status": "running"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
